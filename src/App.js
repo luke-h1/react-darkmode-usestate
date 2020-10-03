5455a900e2f4110242e9e7143bbab2b7f9a5d956
luke-h1 <luke.howsam@yahoo.com>
2020-10-03 12:50:44 +0100

    fix style issues

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Darkmode from './Darkmode';
+function App() {
+  return (
+    <div className="container">
+      <Darkmode />
+    </div>
+  );
+}
+
+export default App;
